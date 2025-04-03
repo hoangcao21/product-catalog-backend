e5a79e4c6bb40c4a3935c662f3e0bacf9b118b12
hoangcao21 <hoangcao21@github.com>
2025-04-03 14:17:36 +0700

    chore: add shared http codes

---
+++ src/shared/errors/http.ts
@@ -0,0 +1,35 @@
+import { APIGatewayProxyResult } from 'aws-lambda';
+import HttpStatus from 'http-status';
+
+export class HttpError extends Error {
+  constructor(
+    public statusCode: number,
+    public code: string = 'GENERIC_ERROR',
+    message: string,
+    public cause?: object,
+  ) {
+    super(message);
+  }
+
+  toResponse(): APIGatewayProxyResult {
+    return {
+      statusCode: this.statusCode,
+      body: JSON.stringify({
+        code: this.code,
+        message: this.message,
+        cause: this.cause,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    };
+  }
+}
+
+export class BadRequestError extends HttpError {
+  constructor(
+    public code: string = 'GENERIC_ERROR',
+    message: string,
+    public cause?: object,
+  ) {
+    super(HttpStatus.BAD_REQUEST, code, message, cause);
+  }
+}
