34d60bd0cd75c8730fc23ef51eaeb59293b33e4c
hoangcao21 <hoangcao21@github.com>
2025-04-03 14:18:15 +0700

    chore: add shared cursor utils

--- src/shared/cursor.ts
+++ src/shared/cursor.ts
@@ -1,12 +1,22 @@
 import { ObjectType } from 'dynamoose/dist/General';
 
+export type DynamooseCursor = ObjectType;
+
 export const CursorUtils = {
-  toBase64(value: ObjectType): string {
+  toBase64(value: DynamooseCursor): string {
+    if (!value) {
+      return undefined;
+    }
+
     const jsonString: string = JSON.stringify(value);
 
     return Buffer.from(jsonString).toString('base64');
   },
-  toObject(stringInBase64: string): ObjectType {
+  toCursorObject(stringInBase64: string): DynamooseCursor {
+    if (!stringInBase64) {
+      return undefined; // First page
+    }
+
     const asciiString: string = Buffer.from(stringInBase64, 'base64').toString(
       'ascii',
     );
